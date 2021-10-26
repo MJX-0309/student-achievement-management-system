b4711b213d6ce520684b73eb999b5e69d83c7fc5
MJX <1585225345@qq.com>
2021-10-26 10:15:44 +0800

    补充swagger配置

---
+++ src/main/java/edu/zhku/boot/controller/ScoreController.java
@@ -0,0 +1,14 @@
+package edu.zhku.boot.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author MJX
+ * @date 2021/10/26
+ */
+@Api("成绩管理Api")
+@RequestMapping("/score")
+public class ScoreController {
+
+}
